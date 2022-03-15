b2bd01a09385f7887cfb5e8abfbc7d5509296c3f
Lautaro2017 <lautarogutierrez4@gmail.com>
2022-03-15 10:19:14 -0300

    Create loadParameters.js

---
+++ loadParameters.js
@@ -0,0 +1,6 @@
+debugger;
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const leadId = urlParams.get('id');
+let idInput = document.getElementById('00N6g00000VFlh4');
+idInput.value = leadId;
