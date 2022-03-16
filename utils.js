f444f105e8521891346e1ffe1f7ed1ad886a90be
Lautaro2017 <lautarogutierrez4@gmail.com>
2022-03-16 09:44:58 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,6 +2,6 @@ function loadParameters(){
   debugger;
   const queryString = window.location.search;
   let leadId = queryString.split('id=')[1];
-  let idInput = document.getElementById('00N6g00000VFlh4');
+  let idInput = document.getElementsByName('discordId')[0];
   idInput.value = leadId;
 }
